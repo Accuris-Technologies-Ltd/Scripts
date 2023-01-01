644a3b04b2743fbf8f0fc92a1af8c89c2029f41f
Peter Potvin <80182836+peter-potvin@users.noreply.github.com>
2023-01-01 03:52:37 -0500

    Initial commit

---
+++ arin-irr-route.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+import argparse
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set the API key
+api_key = os.getenv("ARIN_API_KEY")
+
+# Parse the command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("route", help="The IPv4 route to be added to the IRR record")
+parser.add_argument("originas", help="The source ASN for the route")
+args = parser.parse_args()
+
+# Set the route and source ASN from the command-line arguments
+route = args.route
+originas = args.originas
+
+# Set the IRR record parameters
+payload = f"""route: {route}
+origin: AS{originas}
+descr: ANY ABUSE SHOULD BE REPORTED TO contact@admin.accuristechnologies.ca
+mnt-by: MNT-CANAD-111
+admin-c: NOC33562-ARIN
+tech-c: NOC33562-ARIN
+source: ARIN
+"""
+
+# Set the HTTP headers
+headers = {
+    "Content-Type": "application/rpsl",
+    "Accept": "application/rpsl"
+}
+
+# Make the API request to create the IRR record
+response = requests.post(f"https://reg.arin.net/rest/irr/route/{route}/AS{originas}?apikey={api_key}", headers=headers, data=payload)
+
+# Check the API response
+if response.status_code == 200:
+    print("IRR record created successfully")
+else:
+    print("Error creating IRR record: {}".format(response.text))
---
+++ krill-rpki-add.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+import argparse
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+krill_url = os.getenv("KRILL_URL")
+krill_ca = os.getenv("KRILL_CA")
+api_key = os.getenv("KRILL_API_KEY")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("route", help="The IPv4 route to be added to the ROA record")
+parser.add_argument("originas", help="The source ASN for the route")
+args = parser.parse_args()
+
+# Set the route and source ASN from the command-line arguments
+route = args.route
+originas = args.originas
+
+# Set the HTTP headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + str(api_key)
+}
+
+data = {
+    "added": [
+    {
+        "asn": int(originas),
+        "prefix": route
+    }
+    ],
+    "removed": []
+}
+
+# Make the API request to create the IRR record
+response = requests.post(f"https://{krill_url}/api/v1/cas/{krill_ca}/routes", headers=headers, json=data)
+
+# Check the API response
+if response.status_code == 200:
+    print("ROA created successfully")
+else:
+    print("Error creating ROA: {}".format(response.text))
